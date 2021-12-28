8977f6ca914dad66a342fb994870ffa89bbc9231
Teo Podobnik <tp4348@student.uni-lj.si>
2021-12-28 23:04:58 +0100

    configuration

--- api.py
+++ api.py
@@ -1,17 +1,65 @@
 from flask import Flask
+from flask_restx import Resource, Api, fields, reqparse, abort, marshal, marshal_with
 import logging
 import subprocess
-from flask_restful import Resource, Api, reqparse, abort, marshal, fields
 from configparser import ConfigParser
 import psycopg2 as pg
+from psycopg2 import extensions
 from healthcheck import HealthCheck, EnvironmentDump
 from prometheus_flask_exporter import PrometheusMetrics
+from fluent import handler
+import os
+import json
+
+app = Flask(__name__)
+
+# Load configurations from the config file
+def load_configurations():
+    app.config.from_file('config.json', load=json.load)
+
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        # Override variables defined in the environment over the ones from the config file
+        for item in data:
+            if os.environ.get(item):
+                app.config[item] = os.environ.get(item)
+
+load_configurations()
+
+custom_format = {
+  'name': '%(name_of_service)s',
+  'method': '%(crud_method)s',
+  'traffic': '%(directions)s',
+  'type': '%(levelname)s',
+}
+logging.basicConfig(level=logging.INFO)
+l = logging.getLogger('fluent.test')
+h = handler.FluentHandler('Placila', host=app.config["FLUENT_IP"], port=app.config["FLUENT_PORT"])
+formatter = handler.FluentRecordFormatter(custom_format)
+h.setFormatter(formatter)
+l.addHandler(h)
+l.info("Pripravljanje Placila Mikrostoritve", extra={"name_of_service": "Placila", "crud_method": None, "directions": None})
+
+def connect_to_database():
+    return pg.connect(database=app.config["DATABASE_NAME"], user=app.config["DATABASE_USER"], password=app.config["DATABASE_PASSWORD"],
+                      port=app.config["DATABASE_PORT"], host=app.config["DATABASE_IP"])
+
+
+api = Api(app, version='1.0', title='Placila API', description='Abstrakt Placila API',default_swagger_filename='openapi.json', default='Placila CRUD', default_label='koncne tocke in operacije')
+placiloApiModel = api.model('ModelPlacila', {
+    "id": fields.Integer(readonly=True, description='ID placila'),
+    "id_placnika": fields.Integer(readonly=True, description='ID placnika placila'),
+    "id_prejemnika": fields.Integer(readonly=True, description='ID prejemnika placila'),
+    "status": fields.String(readonly=True, description='Status placila')
+})
+placilaApiModel = api.model('ModelPlacil', {"placila": fields.List(fields.Nested(placiloApiModel))})
+ns = api.namespace('Placila CRUD', description='Placila koncne tocke in operacije')
+posodobiModel = api.model('PosodobiPlacilo', {
+    "atribut": fields.String,
+    "vrednost": fields.String
+})
 
 def create_app():
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
-    logger = logging.getLogger(__name__)
-    app = Flask(__name__)
-    api = Api(app)
     metrics = PrometheusMetrics(app)
     health = HealthCheck()
     envdump = EnvironmentDump()
@@ -23,10 +71,8 @@ def create_app():
     api.add_resource(ListPlacil, "/placila")
     api.add_resource(Placilo, "/placila/<int:id>")
 
-    return app
-
 def test_microservice():
-    result = subprocess.Popen('python api_test.py', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen('python api_test.py', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     if 'OK' in str(stderr):
         return 200
@@ -34,7 +80,7 @@ def test_microservice():
         return 409
 
 def check_database_connection():
-    conn = pg.connect('')
+    conn = connect_too_database()
     if conn.poll() == extensions.POLL_OK:
         print ("POLL: POLL_OK")
     elif conn.poll() == extensions.POLL_READ:
@@ -49,6 +95,13 @@ def application_data():
     return {"maintainer": "Teodor Janez Podobnik",
             "git_repo": "https://github.com/Paketi-org/PlacilaStoritve.git"}
 
+class PlaciloModel:
+    def __init__(self, id, id_placnika, id_prejemnika, status):
+        self.id = id
+        self.id_placnika = id_placnika
+        self.id_prejemnika = id_prejemnika
+        self.status = status
+
 placilaPolja = {
     "id": fields.Integer,
     "id_placnika": fields.Integer,
@@ -57,9 +110,9 @@ placilaPolja = {
 }
 
 class Placilo(Resource):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.table_name = 'placila'
-        self.conn = pg.connect('')
+        self.conn = connect_to_database()
         self.cur = self.conn.cursor()
 
         self.parser = reqparse.RequestParser()
@@ -70,9 +123,15 @@ class Placilo(Resource):
         self.parser.add_argument("atribut", type=str)
         self.parser.add_argument("vrednost", type=str)
 
-        super(Placilo, self).__init__()
+        super(Placilo, self).__init__(*args, **kwargs)
 
+    @marshal_with(placiloApiModel)
+    @ns.response(404, 'Placilo ni najden')
+    @ns.doc("Vrni placilo")
     def get(self, id):
+        """
+        Vrni podatke placila glede na ID
+        """
         self.cur.execute("SELECT * FROM placila WHERE id = %s" % str(id))
         row = self.cur.fetchall()
 
@@ -83,18 +142,54 @@ class Placilo(Resource):
         for el, k in zip(row[0], placilaPolja):
             d[k] = el
 
-        return{"placilo": marshal(d, placilaPolja)}
+        placilo = PlaciloModel(
+            id = d["id"],
+            id_placnika = d["id_placnika"],
+            id_prejemnika = d["id_prejemnika"],
+            status = d["status"].strip())
 
+        return placilo, 200
+
+    @marshal_with(placiloApiModel)
+    @ns.expect(posodobiModel)
+    @ns.response(404, 'Placilo ni najden')
+    @ns.doc("Vrni placilo")
     def put(self, id):
+        """
+        Posodobi podatke placila glede na ID
+        """
+        l.info("Posodobi placilo z ID %s" % str(id), extra={"name_of_service": "Placila", "crud_method": "put", "directions": "in"})
         args = self.parser.parse_args()
         attribute = args["atribut"]
         value = args["vrednost"]
         self.cur.execute("""UPDATE {0} SET {1} = '{2}' WHERE id = {3}""".format(self.table_name, attribute, value, id))
         self.conn.commit()
 
-        return 200
+        self.cur.execute("SELECT * FROM placila WHERE id = %s" % str(id))
+        row = self.cur.fetchall()
 
+        if(len(row) == 0):
+            abort(404)
+
+        d = {}
+        for el, k in zip(row[0], placilaPolja):
+            d[k] = el
+
+        placilo = PlaciloModel(
+            id = d["id"],
+            id_placnika = d["id_placnika"],
+            id_prejemnika = d["id_prejemnika"],
+            status = d["status"].strip())
+
+        return placilo, 200
+
+    @ns.doc("Izbrisi placilo")
+    @ns.response(404, 'Placilo ni najdeno')
+    @ns.response(204, 'Placilo izbrisano')
     def delete(self, id):
+        '''
+        Izbrisi placilo glede na ID
+        '''
         self.cur.execute("SELECT * FROM placila")
         rows = self.cur.fetchall()
         ids = []
@@ -110,9 +205,9 @@ class Placilo(Resource):
         return 201
 
 class ListPlacil(Resource):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.table_name = 'placila'
-        self.conn = pg.connect('')
+        self.conn = connect_to_database()
         self.cur = self.conn.cursor()
         self.cur.execute("select exists(select * from information_schema.tables where table_name=%s)", (self.table_name,))
         if self.cur.fetchone()[0]:
@@ -130,7 +225,14 @@ class ListPlacil(Resource):
         self.parser.add_argument("id_prejemnika", type=int, required=True, help="ID prejemnika je obvezen")
         self.parser.add_argument("status", type=str, required=True, help="Status plačila je obvezen")
 
+        super(ListPlacil, self).__init__(*args, **kwargs)
+
+    @ns.marshal_list_with(placilaApiModel)
+    @ns.doc("Vrni vsa placila")
     def get(self):
+        '''
+        Vrni vsa placila
+        '''
         self.cur.execute("SELECT * FROM placila")
         rows = self.cur.fetchall()
         ds = {}
@@ -141,9 +243,24 @@ class ListPlacil(Resource):
                 ds[i][k] = el
             i += 1
 
-        return{"placila": [marshal(d, placilaPolja) for d in ds.values()]}
+        placila = []
+        for d in ds:
+            placilo = PlaciloModel(
+                id = ds[d]["id"],
+                id_placnika = ds[d]["id_placnika"],
+                id_prejemnika = ds[d]["id_prejemnika"],
+                status = ds[d]["status"].strip())
+            placila.append(placilo)
 
+        return {"placila": placila}, 200
+
+    @marshal_with(placiloApiModel)
+    @ns.expect(placiloApiModel)
+    @ns.doc("Dodaj placilo")
     def post(self):
+        '''
+        Dodaj novo placilo
+        '''
         args = self.parser.parse_args()
         values = []
         for a in args.values():
@@ -151,16 +268,17 @@ class ListPlacil(Resource):
         self.cur.execute('''INSERT INTO {0} (id, id_placnika, id_prejemnika, status)
                 VALUES ({1}, {2}, {3}, '{4}')'''.format('placila', *values))
         self.conn.commit()
-        placilo = {
-            "id": args["id"],
-            "id_placnika": args["id_placnika"],
-            "id_prejemnika": args["id_prejemnika"],
-            "status": args["status"],
-        }
 
-        return{"placilo": marshal(placilo, placilaPolja)}, 201
+        placilo = PlaciloModel(
+            id = args["id"],
+            id_placnika = args["id_placnika"],
+            id_prejemnika = args["id_prejemnika"],
+            status = args["status"].strip())
+
+        return placilo, 201 
 
 
 if __name__ == "__main__":
-    app = create_app()
+    create_app()
     app.run(host="0.0.0.0", port=5002)
+    h.close()
